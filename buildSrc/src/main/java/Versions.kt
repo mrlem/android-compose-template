651435bcb0aab64c7cbeb5b3e07b779b8885d44f
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2022-01-08 17:56:18 +0100

    build: bump compose / junit versions

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,6 +4,6 @@ object Versions {
         const val min = 21
         const val target = 31
     }
-    const val compose = "1.0.1"
+    const val compose = "1.0.5"
     const val kotlin = "1.5.31"
 }
