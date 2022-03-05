a4ea3ffb17badc6b402c9739c2efd33bf4471413
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2022-03-05 18:31:52 +0100

    arch: merge feature dagger modules in a single file

--- feature/greeting/data/src/main/kotlin/org/mrlem/sample/compose/feature/greeting/data/GreetingDatabase.kt
+++ feature/greeting/data/src/main/kotlin/org/mrlem/sample/compose/feature/greeting/data/GreetingDatabase.kt
@@ -1,22 +1,9 @@
 package org.mrlem.sample.compose.feature.greeting.data
 
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import org.mrlem.sample.compose.feature.greeting.data.dao.GreetingDao
 
 interface GreetingDatabase {
 
     fun greetingDao(): GreetingDao
-
-}
-
-@InstallIn(SingletonComponent::class)
-@Module
-class DatabaseModule {
-
-    @Provides
-    fun provideGreetingDao(database: GreetingDatabase) = database.greetingDao()
 
 }
--- feature/greeting/data/src/main/kotlin/org/mrlem/sample/compose/feature/greeting/data/Module.kt
+++ feature/greeting/data/src/main/kotlin/org/mrlem/sample/compose/feature/greeting/data/Module.kt
@@ -2,16 +2,28 @@ package org.mrlem.sample.compose.feature.greeting.data
 
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dagger.hilt.migration.DisableInstallInCheck
+import org.mrlem.sample.compose.feature.greeting.data.Module.Bindings
 import org.mrlem.sample.compose.feature.greeting.domain.repository.GreetingRepository
 import org.mrlem.sample.compose.feature.greeting.data.repository.GreetingRepositoryImpl
 
 @InstallIn(SingletonComponent::class)
-@Module
-abstract class Module {
+@Module(includes = [ Bindings::class ])
+class Module {
 
-    @Binds
-    abstract fun provide(impl: GreetingRepositoryImpl): GreetingRepository
+    @Module
+    @DisableInstallInCheck
+    interface Bindings {
+
+        @Binds
+        fun provideGreetingRepository(impl: GreetingRepositoryImpl): GreetingRepository
+
+    }
+
+    @Provides
+    fun provideGreetingDao(database: GreetingDatabase) = database.greetingDao()
 
 }
