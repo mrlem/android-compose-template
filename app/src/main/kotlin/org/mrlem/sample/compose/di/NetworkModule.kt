42733f12f08ce7ca3da9aa7618c1e0f174b91d97
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2022-03-06 14:47:25 +0100

    di: split MainModule

--- app/src/main/kotlin/org/mrlem/sample/compose/di/NetworkModule.kt
+++ app/src/main/kotlin/org/mrlem/sample/compose/di/NetworkModule.kt
@@ -1,7 +1,6 @@
-package org.mrlem.sample.compose
+package org.mrlem.sample.compose.di
 
 import android.content.Context
-import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,7 +14,7 @@ import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-class MainModule {
+class NetworkModule {
 
     companion object {
         private const val READ_TIMEOUT = 30
@@ -23,16 +22,6 @@ class MainModule {
         private const val CONNECTION_TIMEOUT = 10
         private const val CACHE_SIZE_BYTES = 10 * 1024 * 1024L // 10 MB
     }
-
-    @Provides
-    @Singleton
-    fun database(@ApplicationContext appContext: Context): MainDatabase =
-        Room.databaseBuilder(
-            appContext,
-            MainDatabase::class.java,
-            "sample",
-        )
-            .build()
 
     @Provides
     @Singleton
