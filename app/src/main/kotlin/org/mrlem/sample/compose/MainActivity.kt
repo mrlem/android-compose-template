cb7389675ffcae850b9682aae6cc9338ba5ac139
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2022-03-07 10:40:39 +0100

    cleanup: remove warnings

--- app/src/main/kotlin/org/mrlem/sample/compose/MainActivity.kt
+++ app/src/main/kotlin/org/mrlem/sample/compose/MainActivity.kt
@@ -50,7 +50,6 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                     }
-
                 }
             }
         }
--- feature/filmslist/ui/src/main/kotlin/org/mrlem/sample/compose/feature/filmslist/ui/FilmsListLayout.kt
+++ feature/filmslist/ui/src/main/kotlin/org/mrlem/sample/compose/feature/filmslist/ui/FilmsListLayout.kt
@@ -1,8 +1,5 @@
 package org.mrlem.sample.compose.feature.filmslist.ui
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,12 +9,10 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.mrlem.sample.compose.design.theme.ComposeSampleTheme
-import org.mrlem.sample.compose.design.theme.Typography
 import org.mrlem.sample.compose.feature.ghibli.domain.model.Film
 
 @Composable
