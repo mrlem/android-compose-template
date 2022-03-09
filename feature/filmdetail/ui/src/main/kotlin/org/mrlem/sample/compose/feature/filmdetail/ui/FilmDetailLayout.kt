f032885d50a35db7d532c68a3e76a3f6f41fe1db
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2022-03-09 12:15:18 +0100

    filmdetail: split layout composable

--- feature/filmdetail/ui/src/main/kotlin/org/mrlem/sample/compose/feature/filmdetail/ui/FilmDetailLayout.kt
+++ feature/filmdetail/ui/src/main/kotlin/org/mrlem/sample/compose/feature/filmdetail/ui/FilmDetailLayout.kt
@@ -1,22 +1,15 @@
 package org.mrlem.sample.compose.feature.filmdetail.ui
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import org.mrlem.sample.compose.design.theme.ComposeSampleTheme
-import org.mrlem.sample.compose.design.theme.Typography
 
 @Composable
 fun FilmDetailLayout(
@@ -35,109 +28,21 @@ fun FilmDetailLayout(
                 modifier = Modifier
                     .height(titleHeight),
             ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                ) {
-                    AsyncImage(
-                        model = state.image.toString(),
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .fillMaxSize(),
-                    )
-
-                    Text(
-                        text = state.title,
-                        style = Typography.subtitle1,
-                        modifier = Modifier
-                            .padding(32.dp)
-                            .align(Alignment.BottomCenter),
-                    )
-
-                    IconButton(
-                        onClick = { onBackClick() },
-                        modifier = Modifier
-                            .padding(top = 24.dp)
-                            .align(Alignment.TopStart),
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.ic_back),
-                            contentDescription = stringResource(R.string.filmdetail_back),
-                        )
-                    }
-                }
+                FilmDetailHeader(
+                    title = state.title,
+                    image = state.image,
+                    onBackClick = onBackClick,
+                )
             }
         },
     ) {
-
-        val scrollableState = rememberScrollState()
-
-        Column(
-            modifier = Modifier
-                .verticalScroll(scrollableState)
-                .padding(16.dp)
-                .fillMaxSize(),
-        ) {
-
-            Text(
-                text = state.originalTitle,
-                color = Color.LightGray,
-                style = Typography.caption,
-            )
-            Text(
-                text = state.originalTitleRomanised,
-                color = Color.LightGray,
-                style = Typography.caption,
-            )
-
-            Text(
-                text = stringResource(R.string.filmdetail_summary),
-                color = Color.DarkGray,
-                style = Typography.subtitle2,
-                modifier = Modifier
-                    .padding(top = 16.dp),
-            )
-            Text(
-                text = state.summary,
-                textAlign = TextAlign.Justify,
-                color = Color.DarkGray,
-                style = Typography.body2,
-                modifier = Modifier
-                    .padding(top = 16.dp)
-                    .fillMaxWidth(),
-            )
-            Row(
-                modifier = Modifier
-                    .padding(top = 16.dp),
-            ) {
-                Text(
-                    text = stringResource(R.string.filmdetail_date),
-                    style = Typography.body1,
-                )
-                Text(
-                    text = state.releaseDate,
-                    style = Typography.body2,
-                    modifier = Modifier
-                        .padding(start = 4.dp),
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .padding(top = 4.dp),
-            ) {
-                Text(
-                    text = stringResource(R.string.filmdetail_director),
-                    style = Typography.body1,
-                )
-                Text(
-                    text = state.director,
-                    style = Typography.body2,
-                    modifier = Modifier
-                        .padding(start = 4.dp),
-                )
-            }
-        }
+        FilmDetailContent(
+            originalTitle = state.originalTitle,
+            originalTitleRomanised = state.originalTitleRomanised,
+            summary = state.summary,
+            director = state.director,
+            releaseDate = state.releaseDate,
+        )
     }
 
     Box(
@@ -149,7 +54,7 @@ fun FilmDetailLayout(
             onClick = { onFavoriteClick() },
             modifier = Modifier
                 .align(Alignment.CenterEnd)
-                .padding(top = titleHeight - 32.dp),
+                .offset(y = titleHeight - 32.dp),
         ) {
             Icon(
                 painter = painterResource(state.favoriteDrawable),
