45a041f36b4c378a23d694bbab7a9f8563cdb110
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2022-05-03 14:51:17 +0200

    build: upgrade android gradle plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("gradle-plugin","1.6.10"))
-    implementation("com.android.tools.build:gradle:7.1.2")
+    implementation("com.android.tools.build:gradle:7.1.3")
     implementation("com.squareup:javapoet:1.13.0") // FIXME https://github.com/google/dagger/issues/3068
 }
