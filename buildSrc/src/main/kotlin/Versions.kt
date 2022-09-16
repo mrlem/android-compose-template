0b5d6cc2e362f17b8f28934cdf9bf477dde5cd1b
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2022-09-16 10:06:13 +0200

    build: bump dependencies versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,12 +2,12 @@ object Versions {
 
     object AndroidX {
 
-        const val activity = "1.4.0"
-        const val appcompat = "1.4.1"
-        const val core = "1.7.0"
-        const val lifecycle = "2.4.1"
-        const val navigation = "2.4.2"
-        const val room = "2.4.2"
+        const val activity = "1.5.1"
+        const val appcompat = "1.5.1"
+        const val core = "1.9.0"
+        const val lifecycle = "2.5.1"
+        const val navigation = "2.5.2"
+        const val room = "2.4.3"
 
         object Test {
 
@@ -26,33 +26,33 @@ object Versions {
 
     object Kotlinx {
 
-        const val coroutines = "1.6.3"
+        const val coroutines = "1.6.4"
 
     }
 
     object Sdk {
 
-        const val compile = 32
+        const val compile = 33
         const val min = 21
-        const val target = 32
+        const val target = 33
 
     }
 
     object Compose {
 
-        const val compiler = "1.2.0"
-        const val compose = "1.2.0-rc03"
+        const val compiler = "1.3.1"
+        const val compose = "1.2.1"
         const val uiTooling = "1.1.1"
 
     }
 
-    const val accompanist = "0.24.13-rc"
+    const val accompanist = "0.25.1"
     const val chucker = "3.5.2"
-    const val coil = "2.1.0"
+    const val coil = "2.2.1"
     const val junit = "4.13.2"
-    const val hilt = "2.42"
-    const val kotlin = "1.7.0"
-    const val material = "1.5.0"
+    const val hilt = "2.43.2"
+    const val kotlin = "1.7.10"
+    const val material = "1.6.1"
     const val retrofit = "2.9.0"
     const val timber = "5.0.1"
     const val versions = "0.42.0"
