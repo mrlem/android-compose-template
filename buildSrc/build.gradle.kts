97cc1a90a3e219afd63870db1403e72a3a1a7c98
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2023-02-19 10:47:27 +0100

    build: migrate to gradle 7.5 & gradle plugin 7.4.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("gradle-plugin","1.7.10"))
-    implementation("com.android.tools.build:gradle:7.3.0")
+    implementation("com.android.tools.build:gradle:7.4.1")
     implementation("com.squareup:javapoet:1.13.0") // FIXME https://github.com/google/dagger/issues/3068
 }
