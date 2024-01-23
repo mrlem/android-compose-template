49f03c0174fefd5382b545b8d3c0c9ffbc7ec3a2
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2024-01-23 13:14:32 +0100

    arch: add viewmodels

--- features/home/ui/src/main/java/org/mrlem/sample/compose/features/home/ui/HomeScreen.kt
+++ features/home/ui/src/main/java/org/mrlem/sample/compose/features/home/ui/HomeScreen.kt
@@ -5,12 +5,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.mrlem.sample.compose.core.ui.theme.ComposeSampleTheme
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
     Text(
-        text = "Hello!",
+        text = viewModel.state.label,
         modifier = Modifier
             .fillMaxSize(),
     )
---
+++ features/home/ui/src/main/java/org/mrlem/sample/compose/features/home/ui/HomeViewState.kt
@@ -0,0 +1,5 @@
+package org.mrlem.sample.compose.features.home.ui
+
+data class HomeViewState(
+    val label: String,
+)
