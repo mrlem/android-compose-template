7060a699c0c2aba0b6cfeb575f54a704236e6662
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2024-01-26 09:02:02 +0100

    home: make previews show dark mode

--- features/home/ui/src/main/java/org/mrlem/sample/compose/features/home/ui/HomeScreen.kt
+++ features/home/ui/src/main/java/org/mrlem/sample/compose/features/home/ui/HomeScreen.kt
@@ -1,11 +1,14 @@
 package org.mrlem.sample.compose.features.home.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import org.mrlem.sample.compose.core.ui.base.UiModePreviews
 import org.mrlem.sample.compose.core.ui.theme.ComposeSampleTheme
@@ -21,7 +24,8 @@ private fun Home(state: HomeViewState) {
     Text(
         text = state.label,
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .padding(16.dp),
     )
 }
 
@@ -29,10 +33,12 @@ private fun Home(state: HomeViewState) {
 @Composable
 private fun Preview() {
     ComposeSampleTheme {
-        Home(
-            state = HomeViewState(
-                label = "Plop",
-            ),
-        )
+        Surface {
+            Home(
+                state = HomeViewState(
+                    label = "Plop",
+                ),
+            )
+        }
     }
 }
