0d4140e4ccf3e792b644736b2395c4e1b901ffff
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2024-01-26 07:31:12 +0100

    ui: add day/night preview

---
+++ core/feature/ui/src/main/java/org/mrlem/sample/compose/core/ui/base/UiModePreviews.kt
@@ -0,0 +1,17 @@
+package org.mrlem.sample.compose.core.ui.base
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    name = "Day",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_NO,
+)
+@Preview(
+    name = "Night",
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES,
+)
+annotation class UiModePreviews
