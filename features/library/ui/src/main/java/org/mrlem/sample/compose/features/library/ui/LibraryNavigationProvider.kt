d7053348acd9bcf86cff9e4b8312953b56697ba3
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2024-01-27 15:14:30 +0100

    nav: add library subgraph

--- features/library/ui/src/main/java/org/mrlem/sample/compose/features/library/ui/LibraryNavigationProvider.kt
+++ features/library/ui/src/main/java/org/mrlem/sample/compose/features/library/ui/LibraryNavigationProvider.kt
@@ -3,9 +3,9 @@ package org.mrlem.sample.compose.features.library.ui
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
 import androidx.navigation.NavController
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
 import org.mrlem.sample.compose.core.ui.base.NavBarItem
 import org.mrlem.sample.compose.core.ui.base.NavProvider
 import se.ansman.dagger.auto.AutoBindIntoSet
@@ -23,8 +23,10 @@ class LibraryNavigationProvider @Inject constructor() : NavProvider {
 
     override fun graph(builder: NavGraphBuilder, navController: NavController) =
         builder.run {
-            composable("library") {
-                LibraryScreen()
+            navigation(startDestination = "artists", route = "library") {
+                composable("artists") {
+                    LibraryScreen()
+                }
             }
         }
 
