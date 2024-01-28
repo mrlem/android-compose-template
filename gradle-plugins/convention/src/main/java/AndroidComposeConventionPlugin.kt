86dde9fc61579e793992f9046ec8d744e9228542
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2024-01-28 18:05:11 +0100

    gradle: add timber for logging

--- gradle-plugins/convention/src/main/java/AndroidComposeConventionPlugin.kt
+++ gradle-plugins/convention/src/main/java/AndroidComposeConventionPlugin.kt
@@ -1,9 +1,5 @@
-import com.android.build.api.dsl.ApplicationExtension
-import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.PluginInstantiationException
-import org.gradle.kotlin.dsl.findByType
 import org.mrlem.sample.compose.gradleplugins.commonExtension
 import org.mrlem.sample.compose.gradleplugins.configureAndroidCompose
 
